6262cc2f48609014fa711ce8d5a63f2fad08e495
Yeonghwan Park <vyckd354@gmail.com>
2023-03-09 15:16:57 -0600

    Update test from RE to string

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  render(<App />);  // render 는 인자로 받은 컴포넌트를 가상 Dom에 생성
+  const linkElement = screen.getByText("Learn React"); // screen.getByText는 텍스트를 기반으로 요소를 찾음 (Regular expression 혹은 문자열로)
+  expect(linkElement).toBeInTheDocument(); // 성공과 실패를 확인하는 단언(Assertion)부분. // .toBeInTheDocument 매쳐(Matcher)는 단언(Assertion)부분의 타입(요소가 문서에 있는지)
 });
