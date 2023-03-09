f032e41e515d7f0b45adff745f1a6e14d721ae49
Yeonghwan Park <vyckd354@gmail.com>
2023-03-09 17:37:13 -0600

    add change of button-color and button-name and its test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {  // global test 메서드는 두 인자(argument)를 받음 (string, function)
-  render(<App />);  // render 는 인자로 받은 컴포넌트를 가상 Dom에 생성
-  const linkElement = screen.getByRole('link', { name: /learn react/i } ); // screen.getByText는 텍스트를 기반으로 요소를 찾음 (Regular expression 혹은 문자열로)
-  expect(linkElement).toBeInTheDocument(); // 성공과 실패를 확인하는 단언(Assertion)부분. // .toBeInTheDocument 매쳐(Matcher)는 단언(Assertion)부분의 타입(요소가 문서에 있는지)
+test('Button has correct initial color, and updates when clicked', () => {
+  render(<App />);
+  const colorBtn = screen.getByRole("button", { name: "Change to blue" }); // "Change to blue" 라는 버튼이 있는지
+  expect(colorBtn).toHaveStyle(`background-color: red`)
+
+  fireEvent.click(colorBtn); // 버튼 클릭 발생시
+  expect(colorBtn).toHaveStyle(`background-color: blue`) // 파랑색으로 변경
+  expect(colorBtn).toHaveTextContent('Change to red')
 });
