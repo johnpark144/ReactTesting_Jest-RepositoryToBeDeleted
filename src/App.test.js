b8e2b2604d4db946fc8f9426433eddd677a23e13
Yeonghwan Park <vyckd354@gmail.com>
2023-03-10 15:17:22 -0600

    add userEvent's click in test

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen, fireEvent } from "@testing-library/react";
-import App from "./App";
-import { replaceCamelWithSpaces } from "./App";
-
-test("Button has correct initial color, and updates when clicked", () => {
-  render(<App />);
-  const linkElement = screen.getByText("learn react");
-  expect(linkElement).toBeInTheDocument();
-});
