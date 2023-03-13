52c7e4c026ae99bd8e452e6a9ab64b71b38798c9
Yeonghwan Park <vyckd354@gmail.com>
2023-03-13 14:28:11 -0500

    Test and code alert for server error using waitFor

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -2,18 +2,26 @@ import { useEffect, useState } from "react";
 import ScoopOption from "./ScoopOption";
 import ToppingOption from "./ToppingOption";
 import axios from "axios";
+import AlertBanner from "./../common/AlertBanner";
 
 export default function Options({ optionType }) {
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     axios
       .get(`http://localhost:3030/${optionType}`)
       .then((response) => setItems(response.data))
-      .catch((error) => {});
+      .catch((error) => {
+        setError(true);
+      });
   }, [optionType]);
 
-  const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
+  if (error) {
+    return <AlertBanner />;
+  }
+
+  const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption; // optionType에 따라 다르게 컴포넌트를 불러옴
 
   const optionItems = items.map((item) => (
     <ItemComponent
